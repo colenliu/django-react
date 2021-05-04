024eadb10ddc897e1233468310f548070b6ac525
Colen Liu <colenliu@Colens-MacBook-Pro.local>
2021-05-03 17:08:57 -0700

    commit 7: minor changes

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/colenliu/Projects/replicate-projects/django-react/venv"
+set -gx VIRTUAL_ENV "/Users/colenliu/Projects/django-react/django-react/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
